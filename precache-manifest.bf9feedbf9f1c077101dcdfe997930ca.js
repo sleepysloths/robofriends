5d19e1ee754a73b87dae7e002244c3790ab16d7b
sleepysloths <47724056+sleepysloths@users.noreply.github.com>
2020-09-13 14:37:51 -0700

    Updates

---
+++ precache-manifest.bf9feedbf9f1c077101dcdfe997930ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e725014bffaa0a5edbb29c07a88bb20c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "957938df1e7c5f0d1b18",
+    "url": "/robofriends/static/css/main.8577027e.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "957938df1e7c5f0d1b18",
+    "url": "/robofriends/static/js/main.240113d9.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
